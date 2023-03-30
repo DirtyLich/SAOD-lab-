de039e63654a518eb27576ac7d5f02b3c1e76d05
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-03-30 12:32:08 +0600

    Update laba2.c

--- laba2.c
+++ laba2.c
@@ -1,18 +1,18 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdint.h>
 
-void swap(int *a, int *b) {
-    int temp = *a;
+void swap(uint32_t *a, uint32_t *b) {
+    uint32_t temp = *a;
     *a = *b;
     *b = temp;
 }
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-    for (int j = low; j <= high - 1; j++) {
+uint32_t partition(uint32_t arr[], uint32_t low, uint32_t high) {
+    uint32_t pivot = arr[high];
+    uint32_t i = low - 1;
+    for (uint32_t j = low; j <= high - 1; j++) {
         if (arr[j] < pivot) {
             i++;
             swap(&arr[i], &arr[j]);
@@ -22,19 +22,19 @@ int partition(int arr[], int low, int high) {
     return i + 1;
 }
 
-void quick_sort(int arr[], int low, int high) {
+void quick_sort(uint32_t arr[], uint32_t low, uint32_t high) {
     if (low < high) {
-        int pi = partition(arr, low, high);
+        uint32_t pi = partition(arr, low, high);
         quick_sort(arr, low, pi - 1);
         quick_sort(arr, pi + 1, high);
     }
 }
 
 int main() {
-    int n = 9000000; // Размер массива
-    int* arr = (int*)malloc(n * sizeof(int));
+    uint32_t n = 9000000; // Размер массива
+    uint32_t* arr = (uint32_t*)malloc(n * sizeof(uint32_t));
     srand(time(NULL)); // Инициализация генератора случайных чисел
-    for (int i = 0; i < n; i++) {
+    for (uint32_t i = 0; i < n; i++) {
         arr[i] = rand() % 900000; // Заполнение массива случайными числами в диапазоне от 0 до 1000
     }
     clock_t start = clock(); // Начало замера времени
@@ -42,8 +42,10 @@ int main() {
     clock_t end = clock(); // Конец замера времени
     double time_spent = (double)(end - start) / CLOCKS_PER_SEC; // Вычисление времени работы алгоритма
     printf("Sorted array:\n");
+    for (uint32_t i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     printf("\nTime taken: %f seconds", time_spent);
     free(arr);
     return 0;
 }
-    
