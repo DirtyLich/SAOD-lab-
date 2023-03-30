d18a939d1ee35a1d3400222b826cc0b55dc19f8f
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-03-30 12:49:03 +0600

    Update laba2.c

--- laba2.c
+++ laba2.c
@@ -3,49 +3,44 @@
 #include <time.h>
 #include <stdint.h>
 
-void swap(uint32_t *a, uint32_t *b) {
-    uint32_t temp = *a;
-    *a = *b;
-    *b = temp;
-}
+#define MAX 100000
 
-uint32_t partition(uint32_t arr[], uint32_t low, uint32_t high) {
-    uint32_t pivot = arr[high];
-    uint32_t i = low - 1;
-    for (uint32_t j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
+void quickSort(uint32_t arr[], int left, int right) {
+    int i = left, j = right;
+    uint32_t pivot = arr[(left + right) / 2];
+
+    while (i <= j) {
+        while (arr[i] < pivot) i++;
+        while (arr[j] > pivot) j--;
+        if (i <= j) {
+            uint32_t temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
             i++;
-            swap(&arr[i], &arr[j]);
+            j--;
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    return i + 1;
-}
 
-void quick_sort(uint32_t arr[], uint32_t low, uint32_t high) {
-    if (low < high) {
-        uint32_t pi = partition(arr, low, high);
-        quick_sort(arr, low, pi - 1);
-        quick_sort(arr, pi + 1, high);
-    }
+    if (left < j) quickSort(arr, left, j);
+    if (i < right) quickSort(arr, i, right);
 }
 
 int main() {
-    uint32_t n = 9000000; // Размер массива
-    uint32_t* arr = (uint32_t*)malloc(n * sizeof(uint32_t));
-    srand(time(NULL)); // Инициализация генератора случайных чисел
-    for (uint32_t i = 0; i < n; i++) {
-        arr[i] = rand() % 900000; // Заполнение массива случайными числами в диапазоне от 0 до 1000
+    uint32_t arr[MAX];
+    clock_t start, end;
+
+    srand(time(NULL));
+    for (int i = 0; i < MAX; i++) {
+        arr[i] = rand() % 100001; // заполняем массив псевдослучайными числами из интервала [0,100000]
     }
-    clock_t start = clock(); // Начало замера времени
-    quick_sort(arr, 0, n - 1);
-    clock_t end = clock(); // Конец замера времени
-    double time_spent = (double)(end - start) / CLOCKS_PER_SEC; // Вычисление времени работы алгоритма
+
+    start = clock();
+    quickSort(arr, 0, MAX - 1);
+    end = clock();
+
     printf("Sorted array:\n");
-    for (uint32_t i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\nTime taken: %f seconds", time_spent);
-    free(arr);
+   
+
+    printf("\n\nExecution time: %f seconds", (double)(end - start) / CLOCKS_PER_SEC);
     return 0;
 }
