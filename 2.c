46306cef55dce60749f97743c48bbeddd98b7dd5
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-04-07 08:24:15 +0700

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdint.h>
+#include <stdio.h> //подключаем библиотеку ввода/вывода данных.
+#include <stdlib.h> //подключаем библиотеку стандартных функций, таких как malloc и free.
+#include <time.h> //подключаем библиотеку для работы с временем.
+
+#include <stdint.h> //подключаем библиотеку для работы с целочисленными типами данных фиксированной длины.
 
 #define MAX 100000
-
+//объявляем функцию быстрой сортировки с тремя параметрами: массивом, левой и правой границами подмассива.
 void quickSort(uint32_t arr[], int left, int right) {
     int i = left, j = right;
-    uint32_t pivot = arr[(left + right) / 2];
+    uint32_t pivot = arr[(left + right) / 2]; // выбираем опорный элемент - средний элемент массива
 
     while (i <= j) {
-        while (arr[i] < pivot) i++;
-        while (arr[j] > pivot) j--;
-        if (i <= j) {
+        while (arr[i] < pivot) i++; // ищем элемент, больший или равный опорному, слева
+        while (arr[j] > pivot) j--; // ищем элемент, меньший или равный опорному, справа
+        if (i <= j) { // если найдены элементы, не находящиеся на своих местах, меняем их местами
             uint32_t temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
@@ -21,8 +22,8 @@ void quickSort(uint32_t arr[], int left, int right) {
         }
     }
 
-    if (left < j) quickSort(arr, left, j);
-    if (i < right) quickSort(arr, i, right);
+    if (left < j) quickSort(arr, left, j); // рекурсивно сортируем левую часть массива
+    if (i < right) quickSort(arr, i, right); // рекурсивно сортируем правую часть массива
 }
 
 int main() {
@@ -35,12 +36,17 @@ int main() {
     }
 
     start = clock();
-    quickSort(arr, 0, MAX - 1);
+    quickSort(arr, 0, MAX - 1); // вызываем функцию быстрой сортировки
     end = clock();
 
     printf("Sorted array:\n");
    
+    // Вывод отсортированного массива
+    for (int i = 0; i < MAX; i++) {
+        printf("%d ", arr[i]);
+    }
 
-    printf("\n\nExecution time: %f seconds", (double)(end - start) / CLOCKS_PER_SEC);
+    printf("\n\nExecution time: %f seconds", (double)(end - start) / CLOCKS_PER_SEC); // выводим время выполнения программы
+
     return 0;
 }
